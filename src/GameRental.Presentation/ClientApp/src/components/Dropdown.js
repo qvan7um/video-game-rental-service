e05d8918b505aa347b4e35f3031f7b16a0b1a5ad
Thanhnhan411 <thanhnhan41100@gmail.com>
2023-06-04 00:20:31 +0700

    chinh sua lai componant dropdown filter va them component dropdown cho sap xep

--- src/GameRental.Presentation/ClientApp/src/components/Dropdown.js
+++ src/GameRental.Presentation/ClientApp/src/components/Dropdown.js
@@ -1,9 +1,25 @@
 import React, { useState } from 'react';
 import './Dropdown.css';
 
-function Dropdown({Type, Title, content = []}) {
+function Dropdown({Type, Title, content = [][100]}) {
     const [isActive, setIsAtive] = useState(false);
-    const [isActiveSub, setIsAtiveSub] = useState(false);
+    const [isActiveSubGenre, setIsAtiveSubGenre] = useState(false);
+    const [isActiveSubP, setIsAtiveSubP] = useState(false);
+    const [isActiveSubPF, setIsAtiveSubPF] = useState(false);
+    const [isActiveChip, setIsAtiveChip] = useState(false);
+    const [buttonText, setButtonText] = useState("");
+
+    
+    var value='';
+    
+    const showChip = (value) => {
+        setIsAtiveSubGenre(false);
+        setIsAtiveSubP(false);
+        setIsAtiveSubPF(false);
+        setIsAtive(false);
+        setIsAtiveChip(true);
+        setButtonText(value);
+    };
     let titleStyles = ['dropdown']
     if (Type == 'range') {
         titleStyles.push('right')}
@@ -12,16 +28,58 @@ function Dropdown({Type, Title, content = []}) {
         <div className='dropdown-btn' onClick={e => setIsAtive(!isActive)}>{Title} <i className='fas fa-sort-down'/></div>
         {isActive && 
         <div className='dropdown-contents'>
-            <div className='dropdown-item'>
-                {content[0]}
+            <div className='dropdown-item' onClick={e => setIsAtiveSubPF(!isActiveSubPF)}>
+                {content[0][0]}
             </div>
-            <div className='dropdown-item' onClick={e => setIsAtiveSub(!isActiveSub)}>
-                {content[1]}
+            {isActiveSubPF &&
+                <div className='sub-dropdown-contents'>
+                    <div className='dropdown-item' onClick={e => showChip(content[0][1])}>
+                        {content[0][1]}
+                    </div>
+                    <div className='dropdown-item' onClick={e => showChip(content[0][2])}>
+                        {content[0][2]}
                 
+                    </div>
+                    <div className='dropdown-item' onClick={e => showChip(content[0][3])}>
+                        {content[0][3]}
+                    </div>
+                </div>}
+            <div className='dropdown-item' onClick={e => setIsAtiveSubGenre(!isActiveSubGenre)}>
+                {content[1][0]}
+                
+            </div>{isActiveSubGenre &&
+                <div className='sub-dropdown-contents'>
+                    <div className='dropdown-item' onClick={e => showChip(content[1][1])}>
+                        {content[1][1]}
+                    </div>
+                    <div className='dropdown-item' onClick={e => showChip(content[1][2])}>
+                        {content[1][2]}
+                
+                    </div>
+                    <div className='dropdown-item' onClick={e => showChip(content[1][3])}>
+                        {content[1][3]}
+                    </div>
+                </div>}
+            <div className='dropdown-item'  onClick={e => {setIsAtiveSubP(!isActiveSubP) && (value=content[0][1])}}>
+                {content[2][0]}
             </div>
-            <div className='dropdown-item'>
-                {content[2]}
-            </div>
+                {isActiveSubP &&
+                <div className='sub-dropdown-contents'>
+                    <div className='dropdown-item' onClick={e => showChip(content[2][1])}>
+                        {content[2][1]}
+                    </div>
+                    <div className='dropdown-item' onClick={e => showChip(content[2][2])}>
+                        {content[2][2]}
+                
+                    </div>
+                    <div className='dropdown-item' onClick={e => showChip(content[2][3])}>
+                        {content[2][3]}
+                    </div>
+                </div>}
+        </div>}
+        {isActiveChip &&
+        <div className='chip' onClick={e => setIsAtiveChip(!isActiveChip)}>
+            <button className='btn chip-btn'>{buttonText}<i className='fa fa-times'/></button>
         </div>}
     </div>
   )
