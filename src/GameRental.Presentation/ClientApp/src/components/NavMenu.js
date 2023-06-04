1c2d4aa6ee6409033392701a47eb454d1ddf1a0d
levanquandihoc <levanquan051003@gmail.com>
2023-06-04 22:53:42 +0700

    Added Hero Section to Homepage, changed NavMenu

--- src/GameRental.Presentation/ClientApp/src/components/NavMenu.js
+++ src/GameRental.Presentation/ClientApp/src/components/NavMenu.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
+import '../App.css'
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -24,19 +25,19 @@ export class NavMenu extends Component {
   render() {
     return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">GameRental</NavbarBrand>
+        <Navbar className="nav navbar-expand-sm navbar-toggleable-sm ng-white box-shadow" container light>
+          <NavbarBrand className='navbar-logo' tag={Link} to="/">GameRental</NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
             <ul className="navbar-nav flex-grow">
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                <NavLink tag={Link} className="text-light" to="/">Home</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                <NavLink tag={Link} className="text-light" to="/counter">Counter</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                <NavLink tag={Link} className="text-light" to="/games">Games</NavLink>
               </NavItem>
             </ul>
           </Collapse>
