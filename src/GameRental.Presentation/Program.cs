32bedfd34661a52b6ff7cac0fed0e4f77f371391
Mai Anh Quân <108073174+qvan7um@users.noreply.github.com>
2023-06-06 15:13:37 +0700

    Comments cleanup

--- src/GameRental.Presentation/Program.cs
+++ src/GameRental.Presentation/Program.cs
@@ -33,13 +33,6 @@ builder.Services.AddScoped<ISieveProcessor, SieveProcessor>();
 builder.Services.AddScoped<GameService>();
 builder.Services.AddScoped<ContractService>();
 
-// builder.Services.AddScoped<IMongoCollection<Game>>(sp =>
-// {
-//     var settings = sp.GetRequiredService<IOptions<GameRentalDatabaseSettings>>();
-//     var client = new MongoClient(settings.Value.ConnectionString);
-//     var database = client.GetDatabase(settings.Value.DatabaseName);
-//     return database.GetCollection<Game>(settings.Value.GamesCollectionName);
-// });
 builder.Services.AddScoped<IMongoCollection<Game>>(sp =>
 {
     var settings = sp.GetRequiredService<IOptions<GameRentalDatabaseSettings>>();
