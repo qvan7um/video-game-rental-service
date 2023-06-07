bbadf46e71aed76fceeafa946bc67e1adb7110b8
Mai Anh Quân <108073174+qvan7um@users.noreply.github.com>
2023-06-07 22:35:23 +0700

    Added CRUD operations for contracts collection

--- src/GameRental.Logic/Services/ContractService.cs
+++ src/GameRental.Logic/Services/ContractService.cs
@@ -25,5 +25,27 @@ namespace GameRental.Logic.Services
 
             return contracts;
         }
+
+        public async Task<Contract> Get(string id)
+        {
+            var contract = await _contractRepository.GetAsync(id);
+
+            return contract;
+        }
+
+        public async Task Create(Contract newContract)
+        {
+            await _contractRepository.CreateAsync(newContract);
+        }
+
+        public async Task Update(string id, Contract updatedContract)
+        {
+            await _contractRepository.UpdateAsync(id, updatedContract);
+        }
+
+        public async Task Delete(string id)
+        {
+            await _contractRepository.RemoveAsync(id);
+        }
     }
 }
