c581b2ce7fdbea00f4da0796ebbd2532be0c2c0b
Thanhnhan411 <thanhnhan41100@gmail.com>
2023-06-11 17:55:55 +0700

    fix warning

--- src/GameRental.Presentation/ClientApp/src/components/CardItemS.js
+++ src/GameRental.Presentation/ClientApp/src/components/CardItemS.js
@@ -6,7 +6,7 @@ function CardItem() {
   return (
     <div className='card-container'>
         
-            <img src="https://cdn1.epicgames.com/offer/3ddd6a590da64e3686042d108968a6b2/EGS_GodofWar_SantaMonicaStudio_S2_1200x1600-fbdf3cbc2980749091d52751ffabb7b7_1200x1600-fbdf3cbc2980749091d52751ffabb7b7" alt="Game Image"/>
+            <img src="https://cdn1.epicgames.com/offer/3ddd6a590da64e3686042d108968a6b2/EGS_GodofWar_SantaMonicaStudio_S2_1200x1600-fbdf3cbc2980749091d52751ffabb7b7_1200x1600-fbdf3cbc2980749091d52751ffabb7b7" alt="Game"/>
         <div className='title-btn'>
             <h7 className='title'>God Of War</h7>
             
